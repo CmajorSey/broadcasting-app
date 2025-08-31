96ff0e5c1e9842e682e89cc13494236945559803
CMajorSey <cmajorgabriel@gmail.com>
2025-08-31 14:56:46 +0400

    chore: finalize cleanup

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,13 +8,14 @@ import {
 } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDB2mejIIrbi8cDXGanMiSogE9VmG4MsG8",
-  authDomain: "loboard-notifications.firebaseapp.com",
-  projectId: "loboard-notifications",
-  storageBucket: "loboard-notifications.firebasestorage.app",
-  messagingSenderId: "302425553477",
-  appId: "1:302425553477:web:32e35c1c2e22c96793012c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 
